b437a7acf5560aafd5933712b9c637d3292de136
Evgenii <uvaz2014@mail.ru>
2021-10-26 06:12:39 +0300

    Changed to more OOP for uml

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 import math
 from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT  as  NavigationToolbar)
+from abc import ABCMeta, abstractmethod, ABC
 
 _eulerColor = "#d9138a"
 _exactColor = "#12a4d9"
@@ -15,113 +16,108 @@ def f(x):
     return math.exp(2 * x)
 
 
+class Solution(metaclass=ABCMeta):
+    def __init__(self, grid):
+        self.x_points = grid.x_points
+        self.y_points = []
+        self.grid = grid
+
+    @abstractmethod
+    def Calculate_points(self):
+        pass
+
+
+class ExactSolution(Solution):
+    def Calculate_points(self):
+        self.y_points = []
+        self.x_points = self.grid.x_points
+        for i in range(0, self.grid.n):
+            y = f(self.grid.x_points[i])
+            self.y_points.append(y)
+        return self.y_points
+
+
+class EulerSolution(Solution):
+    def Calculate_points(self):
+        self.y_points = []
+        self.x_points = self.grid.x_points
+        y = self.grid.yi
+        self.y_points.append(y)
+        for i in range(1, self.grid.n):
+            y = y + dy(self.grid.x_points[i], y) * self.grid.h
+            self.y_points.append(y)
+        return self.y_points
+
+
 class Grid:
     def __init__(self, mpl_widget):
-        xi = float(mpl_widget.lineEdit.text())
-        xf = float(mpl_widget.lineEdit_3.text())
-        n = int(mpl_widget.lineEdit_4.text())
-        h = float(((xf - xi) / n))
-        x = xi
-        yi = float(mpl_widget.lineEdit_2.text())
-        y = yi
-        x_plot, y_euler, y_analytical, y_euler_error = [], [], [], []
-        x_plot.append(x)
-        y_euler.append(y)
-        y_euler_error.append(0)
-        y_analytical.append(y)
-        for i in range(1, n):
-            y = y + dy(x, y) * h
-            x = x + h
-            x_plot.append(x)
-            y_euler.append(y)
-            func = f(x)
-            y_analytical.append(func)
-            y_euler_error.append(abs(func - y))
-            print(i, "\t%f \t %f" % (x, y))
-        self.x_plot = x_plot
-        self.y_euler = y_euler
-        self.y_analytical = y_analytical
-        self.y_euler_error = y_euler_error
-        self.xi = xi
-        self.xf = xf
-        self.n = n
-        self.h = h
-        self.yi = yi
+        self.xi = None
+        self.xf = None
+        self.n = None
+        self.h = None
+        self.yi = None
+        self.x_points = []
+        self.mpl_widget = mpl_widget
+
+    def Update(self):
+        mpl_widget = self.mpl_widget
+        self.x_points = []
+        self.xi = float(mpl_widget.lineEdit.text())
+        self.xf = float(mpl_widget.lineEdit_3.text())
+        self.n = int(mpl_widget.lineEdit_4.text())
+        self.h = float(((self.xf - self.xi) / self.n))
+        self.yi = float(mpl_widget.lineEdit_2.text())
+        x = self.xi
+        self.x_points.append(x)
+        for i in range(0, self.n - 1):
+            x = x + self.h
+            self.x_points.append(x)
+
+
+class LTE_Grid:
+    def __init__(self):
+        self.x_points = []
+        self.y_euler = []
+
+    def Calculate_LTE(self, exact, euler):
+        self.x_points = exact.x_points
+        self.y_euler = []
+        self.y_euler.append(exact.y_points[0] - euler.y_points[0])
+        n = len(self.x_points)
+        for i in range(0, n - 1):
+            y = abs(exact.y_points[i] - euler.y_points[i])
+            self.y_euler.append(y)
+        # print("LTE:", self.x_points)
+        # print(self.y_euler)
+        # print(exact.y_points)
+        # print(euler.y_points)
+        # print(len(exact.x_points), len(self.y_euler))
+        return self.y_euler
+
+
+class GTE_Grid:
+    def __init__(self, mpl_widget, grid):
+        self.x_points = []
+        self.y_euler = []
         self.n0 = int(mpl_widget.lineEdit_GE_from.text())
         self.N = int(mpl_widget.lineEdit_GE_to.text())
+        self.xi = grid.xi
+        self.xf = grid.xf
+        self.yi = grid.yi
+        self.mpl_widget = mpl_widget
+        self.grid = grid
 
-
-class MatplotlibWidget(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-        loadUi("DE.ui", self)
-        self.grid = Grid(self)
-        self.setWindowTitle("Computational practicum")
-        self.pushButton_plot.clicked.connect(self.update_graph)
-        self.pushButton_global_error.clicked.connect(self.update_global_error)
-        self.checkBox.setChecked(True)
-        self.checkBox.clicked.connect(self.show_analytical)
-        self.checkBox_2.setChecked(True)
-        self.checkBox_2.clicked.connect(self.show_euler)
-        self.addToolBar(NavigationToolbar(self.SolutionsWidget.canvas, self))
-        self.analytical_line = None
-        self.euler_line = None
-        self.euler_line_error = None
-        self.euler_line_global_error = None
-        self.update_graph()
-
-    def show_euler(self):
-        self.euler_line.set_visible(self.checkBox_2.isChecked())
-        self.euler_line_error.set_visible(self.checkBox_2.isChecked())
-        self.euler_line_global_error.set_visible(self.checkBox_2.isChecked())
-        self.checkBox_2.setChecked(self.checkBox_2.isChecked())
-        self.SolutionsWidget.canvas.draw()
-
-    def show_analytical(self):
-        self.analytical_line.set_visible(self.checkBox.isChecked())
-        self.checkBox.setChecked(self.checkBox.isChecked())
-        self.SolutionsWidget.canvas.draw()
-
-    def update_graph(self):
-        # Initializing
-        self.grid = Grid(self)
-        x_plot = self.grid.x_plot
-        y_analytical = self.grid.y_analytical
-        y_euler = self.grid.y_euler
-        y_euler_error = self.grid.y_euler_error
-        self.SolutionsWidget.canvas.axe_1.clear()
-        self.SolutionsWidget.canvas.axe_2.clear()
-        self.SolutionsWidget.canvas.axe_3.clear()
-        self.analytical_line, = self.SolutionsWidget.canvas.axe_1.plot(x_plot, y_analytical,
-                                                                       color=_exactColor,
-                                                                       label="Analytical",
-                                                                       markersize=6)
-        self.euler_line, = self.SolutionsWidget.canvas.axe_1.plot(x_plot, y_euler, ".",
-                                                                  color=_eulerColor,
-                                                                  label="Euler",
-                                                                  markersize=4)
-        self.euler_line_error, = self.SolutionsWidget.canvas.axe_2.plot(x_plot, y_euler_error, ".",
-                                                                        color=_eulerColor,
-                                                                        label="Euler",
-                                                                        markersize=4)
-        self.SolutionsWidget.canvas.axe_1.legend(loc='upper left')
-        self.SolutionsWidget.canvas.axe_1.set_title('Solutions')
-        self.SolutionsWidget.canvas.axe_2.legend(loc='upper left')
-        self.SolutionsWidget.canvas.axe_2.set_title('LTEs')
-        self.update_global_error()
-        self.show_euler()
-        self.show_analytical()
-        self.SolutionsWidget.canvas.draw()
-
-    def update_global_error(self):
-        self.grid = Grid(self)
-        n0 = self.grid.n0
-        N = self.grid.N
+    def Calculate_GTE(self):
+        self.n0 = int(self.mpl_widget.lineEdit_GE_from.text())
+        self.N = int(self.mpl_widget.lineEdit_GE_to.text())
+        n0 = self.n0
+        N = self.N
         xi = self.grid.xi
         xf = self.grid.xf
         yi = self.grid.yi
-        x_plot_global_error, y_euler_global_error = [], []
-        for i in range(n0, N+1):
+        x_points, y_euler_error = [], []
+        for i in range(n0, N + 1):
+            print("H:", xf, xi, i)
             h = float(((xf - xi) / i))
             y = yi
             x = xi
@@ -131,20 +127,101 @@ class MatplotlibWidget(QMainWindow):
                 x = x + h
                 func = f(x)
                 max_y_euler_error = max(abs(func - y), max_y_euler_error)
-            y_euler_global_error.append(max_y_euler_error)
-            x_plot_global_error.append(i)
-        print("ERR:", y_euler_global_error)
-        print("N:", x_plot_global_error)
-        self.SolutionsWidget.canvas.axe_3.clear()
-        self.euler_line_global_error, = self.SolutionsWidget.canvas.axe_3.plot(x_plot_global_error, y_euler_global_error,
-                                                                               ".",
-                                                                               color=_eulerColor,
-                                                                               label="Euler",
-                                                                               markersize=4)
-        self.SolutionsWidget.canvas.axe_3.legend(loc='upper left')
-        self.SolutionsWidget.canvas.axe_3.set_title('GTEs')
-        self.euler_line_global_error.set_visible(self.checkBox_2.isChecked())
-        self.SolutionsWidget.canvas.draw()
+            y_euler_error.append(max_y_euler_error)
+            x_points.append(i)
+        self.x_points = x_points
+        self.y_euler = y_euler_error
+        return x_points, y_euler_error
+
+
+class MatplotlibWidget(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        loadUi("DE.ui", self)
+        self.setWindowTitle("Computational practicum")
+        self.checkBox.setChecked(True)
+        self.checkBox_2.setChecked(True)
+        self.addToolBar(NavigationToolbar(self.SolutionsWidget.canvas, self))
+        self.exact_line = None
+        self.euler_line = None
+        self.euler_lte_line = None
+        self.euler_gte_line = None
+        self.grid = Grid(self)
+        self.exactSolution = ExactSolution(self.grid)
+        self.eulerSolution = EulerSolution(self.grid)
+        self.LTE_grid = LTE_Grid()
+        self.GTE_grid = GTE_Grid(self, self.grid)
+        self.controller = Controller(self)
+        self.pushButton_plot.clicked.connect(self.controller.update_graph)
+        self.pushButton_global_error.clicked.connect(self.controller.update_GTE)
+        self.checkBox.clicked.connect(self.controller.show_exact)
+        self.checkBox_2.clicked.connect(self.controller.show_euler)
+        self.controller.update_graph()
+
+
+class Controller:
+    def __init__(self, mpl_widget):
+        self.mpl_widget = mpl_widget
+
+    def show_exact(self):
+        self.mpl_widget.exact_line.set_visible(self.mpl_widget.checkBox.isChecked())
+        self.mpl_widget.checkBox.setChecked(self.mpl_widget.checkBox.isChecked())
+        self.mpl_widget.SolutionsWidget.canvas.draw()
+
+    def show_euler(self):
+        self.mpl_widget.euler_line.set_visible(self.mpl_widget.checkBox_2.isChecked())
+        self.mpl_widget.euler_lte_line.set_visible(self.mpl_widget.checkBox_2.isChecked())
+        self.mpl_widget.euler_gte_line.set_visible(self.mpl_widget.checkBox_2.isChecked())
+        self.mpl_widget.checkBox_2.setChecked(self.mpl_widget.checkBox_2.isChecked())
+        self.mpl_widget.SolutionsWidget.canvas.draw()
+
+    def update_graph(self):
+        mpl_widget = self.mpl_widget
+        mpl_widget.grid.Update()
+        x_points = mpl_widget.grid.x_points
+        y_exact = mpl_widget.exactSolution.Calculate_points()
+        y_euler = mpl_widget.eulerSolution.Calculate_points()
+        y_euler_lte = mpl_widget.LTE_grid.Calculate_LTE(mpl_widget.exactSolution, mpl_widget.eulerSolution)
+        mpl_widget.SolutionsWidget.canvas.axe_1.clear()
+        mpl_widget.SolutionsWidget.canvas.axe_2.clear()
+        mpl_widget.SolutionsWidget.canvas.axe_3.clear()
+        mpl_widget.exact_line, = mpl_widget.SolutionsWidget.canvas.axe_1.plot(x_points, y_exact,
+                                                                              color=_exactColor,
+                                                                              label="Analytical",
+                                                                              markersize=6)
+        mpl_widget.euler_line, = mpl_widget.SolutionsWidget.canvas.axe_1.plot(x_points, y_euler, ".",
+                                                                              color=_eulerColor,
+                                                                              label="Euler",
+                                                                              markersize=4)
+        mpl_widget.euler_lte_line, = mpl_widget.SolutionsWidget.canvas.axe_2.plot(x_points, y_euler_lte, ".",
+                                                                                  color=_eulerColor,
+                                                                                  label="Euler",
+                                                                                  markersize=4)
+        mpl_widget.SolutionsWidget.canvas.axe_1.legend(loc='upper left')
+        mpl_widget.SolutionsWidget.canvas.axe_1.set_title('Solutions')
+        mpl_widget.SolutionsWidget.canvas.axe_2.legend(loc='upper left')
+        mpl_widget.SolutionsWidget.canvas.axe_2.set_title('LTEs')
+        self.update_GTE()
+        self.show_euler()
+        self.show_exact()
+        mpl_widget.SolutionsWidget.canvas.draw()
+
+    def update_GTE(self):
+        mpl_widget = self.mpl_widget
+        mpl_widget.grid.Update()
+        print(mpl_widget.grid.xi)
+        x_points, y_points = mpl_widget.GTE_grid.Calculate_GTE()
+        mpl_widget.SolutionsWidget.canvas.axe_3.clear()
+        mpl_widget.euler_gte_line, = mpl_widget.SolutionsWidget.canvas.axe_3.plot(x_points,
+                                                                                  y_points,
+                                                                                  ".",
+                                                                                  color=_eulerColor,
+                                                                                  label="Euler",
+                                                                                  markersize=4)
+        mpl_widget.SolutionsWidget.canvas.axe_3.legend(loc='upper left')
+        mpl_widget.SolutionsWidget.canvas.axe_3.set_title('GTEs')
+        mpl_widget.euler_gte_line.set_visible(mpl_widget.checkBox_2.isChecked())
+        mpl_widget.SolutionsWidget.canvas.draw()
 
 
 app = QApplication([])
